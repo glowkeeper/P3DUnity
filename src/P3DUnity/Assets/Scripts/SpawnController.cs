41dd11c3cf58bf1282b976b4ea56865dc94b48cc
glowkeeper <steve.huckle@gmail.com>
2023-10-31 17:34:40 +0000

    fixed small canSpawn bug

--- src/P3DUnity/Assets/Scripts/SpawnController.cs
+++ src/P3DUnity/Assets/Scripts/SpawnController.cs
@@ -44,6 +44,7 @@ public class SpawnController : MonoBehaviour
             numThings++;
             return true;
         } else {
+            canSpawn = false;
             return false;
         }
     }
